85979785dcc515f36c4125286c1f056f2987d1db
earl9rey <earl9rey@naver.com>
2026-07-25 17:56:35 +0900

    feat: 정책 신청 가능 여부 판정 로직 추가

---
+++ src/main/java/com/mozip/server/policy/domain/PolicyAvailability.java
@@ -0,0 +1,7 @@
+package com.mozip.server.policy.domain;
+
+public enum PolicyAvailability {
+    AVAILABLE,
+    UNAVAILABLE,
+    NEEDS_REVIEW
+}
---
+++ src/main/java/com/mozip/server/policy/domain/PolicyAvailabilityResult.java
@@ -0,0 +1,4 @@
+package com.mozip.server.policy.domain;
+
+public record PolicyAvailabilityResult(PolicyAvailability status, PolicyAvailabilityReason reason) {
+}
