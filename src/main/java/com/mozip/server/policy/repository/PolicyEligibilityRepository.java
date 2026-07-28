8e17417966226c2772a246abd4aeb8d9031e70cb
earl9rey <earl9rey@naver.com>
2026-07-28 11:11:13 +0900

    feat: 추천 정책 배치 조회 리포지토리 추가

--- src/main/java/com/mozip/server/policy/repository/PolicyEligibilityRepository.java
+++ src/main/java/com/mozip/server/policy/repository/PolicyEligibilityRepository.java
@@ -1,10 +1,13 @@
 package com.mozip.server.policy.repository;
 
 import com.mozip.server.policy.entity.PolicyEligibility;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PolicyEligibilityRepository extends JpaRepository<PolicyEligibility, Long> {
 
     Optional<PolicyEligibility> findByPolicyId(Long policyId);
+
+    List<PolicyEligibility> findByPolicyIdIn(List<Long> policyIds);
 }
