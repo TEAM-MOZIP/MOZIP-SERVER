28da6e8f87ca0264edbdf0e6d91b6cefd37a0d86
earl9rey <earl9rey@naver.com>
2026-07-28 13:03:52 +0900

    feat: 정책 북마크 중복 예외 처리 추가

--- src/main/java/com/mozip/server/global/exception/ErrorCode.java
+++ src/main/java/com/mozip/server/global/exception/ErrorCode.java
@@ -11,6 +11,7 @@ public enum ErrorCode {
     USER_PROFILE_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자 프로필을 찾을 수 없습니다."),
     REGION_NOT_FOUND(HttpStatus.NOT_FOUND, "지역을 찾을 수 없습니다."),
     USER_PROFILE_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 등록된 사용자 프로필입니다."),
+    BOOKMARK_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 북마크한 정책입니다."),
     INVALID_REQUEST(HttpStatus.BAD_REQUEST, "요청 값이 올바르지 않습니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증이 필요합니다."),
     FORBIDDEN(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
