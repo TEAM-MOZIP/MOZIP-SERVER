bc2f159f190040f74eb8645ff37dc54f557f9175
earl9rey <earl9rey@naver.com>
2026-08-06 13:33:35 +0900

    feat: 프로필 등록 요청 DTO의 gender를 Gender enum으로 전환

--- src/main/java/com/mozip/server/user/dto/UserProfileUpdateRequest.java
+++ src/main/java/com/mozip/server/user/dto/UserProfileUpdateRequest.java
@@ -1,9 +1,9 @@
 package com.mozip.server.user.dto;
 
 import com.mozip.server.user.entity.EmploymentStatus;
+import com.mozip.server.user.entity.Gender;
 import com.mozip.server.user.entity.HouseholdType;
 import com.mozip.server.user.entity.IncomeType;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.PositiveOrZero;
@@ -12,8 +12,7 @@ import java.time.LocalDate;
 public record UserProfileUpdateRequest(
         @NotNull @PastOrPresent LocalDate birthDate,
         @NotNull Long regionId,
-        // TODO: 허용값이 확정되면 Enum + @NotNull로 전환 (현재는 자유 문자열 + 공백 검증만)
-        @NotBlank String gender,
+        @NotNull Gender gender,
         @NotNull IncomeType incomeType,
         @NotNull @PositiveOrZero Integer incomeValue,
         @NotNull EmploymentStatus employmentStatus,
