99b8b8ea93e158693b2f4fa0657d9ad51b8f692f
earl9rey <earl9rey@naver.com>
2026-08-21 21:57:01 +0900

    feat: RegionRepository에 code 기반 조회 추가

--- src/main/java/com/mozip/server/region/repository/RegionRepository.java
+++ src/main/java/com/mozip/server/region/repository/RegionRepository.java
@@ -1,7 +1,10 @@
 package com.mozip.server.region.repository;
 
 import com.mozip.server.region.entity.Region;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RegionRepository extends JpaRepository<Region, Long> {
+
+    Optional<Region> findByCode(String code);
 }
